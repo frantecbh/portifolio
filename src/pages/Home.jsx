6b907c239bf7901d312dda1cd70b550020791742
frantecbh <franciscomenezesbh@gmail.com>
2024-02-29 22:56:31 -0300

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Hero } from './sections/Hero'
+
+export function Home() {
+  return <Hero />
+}
---
+++ src/pages/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export function Hero() {
+  return (
+    <div className="bg-slate-800">
+      <img
+        src="https://github.com/frantecbh.png"
+        className="size-96 rounded-full"
+        alt=""
+      />
+    </div>
+  )
+}
